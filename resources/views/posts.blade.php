23b5e2a15c7ed90c3d567c14093e0e88227811b9
Praveen Dias <praveendias1180@gmail.com>
2021-12-09 19:55:36 +0530

    🚀Laravel from scratch.

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Posts</title>
+</head>
+<body>
+    <article>
+        <h1>Hello there</h1> 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare id magna a consectetur. Suspendisse potenti. In eget dignissim risus. Duis magna felis, ullamcorper finibus leo a, ornare pretium arcu. Aenean id consectetur massa. Morbi viverra metus vitae sodales elementum. Donec enim leo, auctor vel scelerisque vel, tristique in mi. Vivamus a elementum libero. Nullam sed semper orci. Curabitur non posuere tellus, eget congue ligula. Integer fermentum risus et tempor bibendum. Sed facilisis ex sed interdum bibendum.
+
+        Nulla varius ex justo, a finibus dui auctor ut. Integer purus eros, venenatis eu enim quis, laoreet gravida nisl. Aliquam venenatis, magna vel interdum sollicitudin, odio orci pharetra eros, a suscipit mauris erat at dolor. Integer tincidunt vulputate facilisis. Donec elementum mi a massa sodales, a finibus tortor commodo. Cras vitae ligula at eros iaculis viverra. Vivamus tincidunt, odio sed aliquam facilisis, orci nulla condimentum eros, at porttitor lorem urna nec lectus. Ut quis sollicitudin dolor, ac imperdiet tortor. Nullam viverra fringilla eros, et venenatis diam ullamcorper ac. Sed quis nulla viverra, luctus dolor et, gravida sapien. Curabitur maximus ante tellus, in posuere odio euismod vitae. Sed aliquet diam nec felis egestas, non vulputate velit venenatis. Nam placerat ac arcu id posuere. Nunc hendrerit risus posuere odio convallis dapibus. Praesent et orci sit amet elit tincidunt euismod.</p>
+    </article>
+
+    <article>
+        <h1>Hello there</h1> 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare id magna a consectetur. Suspendisse potenti. In eget dignissim risus. Duis magna felis, ullamcorper finibus leo a, ornare pretium arcu. Aenean id consectetur massa. Morbi viverra metus vitae sodales elementum. Donec enim leo, auctor vel scelerisque vel, tristique in mi. Vivamus a elementum libero. Nullam sed semper orci. Curabitur non posuere tellus, eget congue ligula. Integer fermentum risus et tempor bibendum. Sed facilisis ex sed interdum bibendum.
+
+        Nulla varius ex justo, a finibus dui auctor ut. Integer purus eros, venenatis eu enim quis, laoreet gravida nisl. Aliquam venenatis, magna vel interdum sollicitudin, odio orci pharetra eros, a suscipit mauris erat at dolor. Integer tincidunt vulputate facilisis. Donec elementum mi a massa sodales, a finibus tortor commodo. Cras vitae ligula at eros iaculis viverra. Vivamus tincidunt, odio sed aliquam facilisis, orci nulla condimentum eros, at porttitor lorem urna nec lectus. Ut quis sollicitudin dolor, ac imperdiet tortor. Nullam viverra fringilla eros, et venenatis diam ullamcorper ac. Sed quis nulla viverra, luctus dolor et, gravida sapien. Curabitur maximus ante tellus, in posuere odio euismod vitae. Sed aliquet diam nec felis egestas, non vulputate velit venenatis. Nam placerat ac arcu id posuere. Nunc hendrerit risus posuere odio convallis dapibus. Praesent et orci sit amet elit tincidunt euismod.</p>
+    </article>
+</body>
+</html>
