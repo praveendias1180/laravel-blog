cb4283247564a69aab4a0aa11554a1fa4926efe1
Praveen Dias <praveendias1180@gmail.com>
2021-12-09 20:04:34 +0530

    posts and post hardcoded templates.

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
 
 Route::get('/posts', function () {
     return view('posts');
+});
+
+Route::get('post', function () {
+    return view('post');
 });
 
