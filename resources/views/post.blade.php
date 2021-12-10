9fb6e0329d08d311d9db2138a37771a9c8ded7f6
Praveen Dias <praveendias1180@gmail.com>
2021-12-10 08:44:56 +0530

    🎉Completed using blade templates.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/app.css">
-    <title><?= $post->title; ?></title>
-</head>
-<body>
+@extends ('layout')
+
+@section ('content')
     <article>
-        <h1><a href="post"><?= $post->title; ?></a></h1> 
-        <?= $post->body; ?>
+        <h1><a href="post">{{ $post->title }}</a></h1>
+        {!! $post->body !!}
     </article>
     <a href="/">Go back</a>
-    
-</body>
-</html>
+@endsection
