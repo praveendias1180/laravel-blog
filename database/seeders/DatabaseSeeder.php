4ece50b29c2f9a973f8b49dd9e35de3891f7c955
Praveen Dias <praveendias1180@gmail.com>
2021-12-10 19:49:35 +0530

    🎉Added DB Seeders.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        Post::create([
+            'title' => 'Personal Post',
+            'category_id' => $personal->id,
+            'user_id' => $user->id,
+            'slug' => 'personal-post',
+            'excerpt' => 'The excerpt of the personal post',
+            'body' => 'The body of the personal post.'
+        ]);
+
+        Post::create([
+            'title' => 'Family Post',
+            'category_id' => $family->id,
+            'user_id' => $user->id,
+            'slug' => 'family-post',
+            'excerpt' => 'The excerpt of the family post',
+            'body' => 'The body of the family post.'
+        ]);
+
+        Post::create([
+            'title' => 'Other Post',
+            'category_id' => $personal->id,
+            'user_id' => $user->id,
+            'slug' => 'other-post',
+            'excerpt' => 'The excerpt of the other post',
+            'body' => 'The body of the other post.'
+        ]);
     }
 }
