efa4ed398474654f73b03a0af724a7795661aabf
Praveen Dias <praveendias1180@gmail.com>
2021-12-13 19:43:20 +0530

    ✔️Posts page completed.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,4 @@
 <x-layout>
-    <x-slot name="title">
-        All Posts
-    </x-slot>
-    <x-slot name="lara">
-        @foreach($posts as $post)
-        <article>
-            <h1><a href="/posts/<?= $post->slug; ?>">{{ $post->title}}</a></h1>
-            <p>By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a></p>
-            <p>In <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-            <p>{{ $post->excerpt }}</p>
-        </article>
-        @endforeach 
-    </x-slot>
     <x-slot name="content">
 
         @include('_posts-header')
