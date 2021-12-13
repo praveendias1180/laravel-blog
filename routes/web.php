847aaa31c624845d785f537c871dccc97c1d30be
Praveen Dias <praveendias1180@gmail.com>
2021-12-13 23:40:14 +0530

    🏁 Implemented drop down component.

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->with('category', 'author')->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -33,12 +34,15 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author']) 
+        'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author']) 
+        'posts' => $author->posts->load(['category', 'author']), 
+        'categories' => Category::all()
     ]);
 });
