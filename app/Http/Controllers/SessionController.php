b0f8f57abe1cdf674eeac261c90505611700e63c
Praveen Dias <praveendias1180@gmail.com>
2021-12-14 16:03:47 +0530

    🚣‍♂️Login Logout implemented.

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    public function create(){
+        return view('session.create');
+    }
+
+    public function store(){
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required|max:255'
+        ]);
+
+        if(auth()->attempt($attributes)){
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+
+        // return back()
+        //     ->withInput()
+        //     ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+    }
+
+    public function destroy(){
+        auth()->logout();
+        return redirect('/')->with('success', 'Goodbye!');
+    } 
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,5 +20,9 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+Route::get('login', [SessionController::class, 'create'])->middleware('guest');
+Route::post('session', [SessionController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
