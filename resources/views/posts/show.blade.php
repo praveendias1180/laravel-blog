adf50780b1dc7e42a1e5d6734c42bd9cd1662d9e
Praveen Dias <praveendias1180@gmail.com>
2021-12-14 21:43:40 +0530

    🟠Looking forward to Mailchimp integration.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -53,30 +53,7 @@
                 </div>
             </div>
             <section class="col-span-8 col-start-5 space-y-6">
-                @auth
-                <x-panel>
-                <form method="POST" action="/posts/{{ $post->slug }}/comments">
-                    @csrf
-                    <header class="flex items-center">
-                        <img src="https://i.pravatar.cc/60?u={{ auth()->user()->id }}" alt="" width="40" height="40" class="rounded-full mr-3"/>
-                        <h2>Want to participate?</h2>
-                    </header>
-                    <div class="mt-6">
-                        <textarea name="body" 
-                            rows="5"
-                            class="w-full focus:outline-none focus:ring" 
-                            placeholder="Say something!" ></textarea>
-                    </div>
-                    <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-600 px-10 py-2 rounded-2xl text-white">Post</button>
-                    </div>
-                </form>
-                </x-panel>
-                @else
-                    <p class="font-semibold">
-                    <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Login</a> to leave a comment.
-                    </p>
-                @endauth
+                @include('posts._add-comment-form');
 
                 @foreach($post->comments as $comment)
                     <x-post-comment :comment="$comment" />
