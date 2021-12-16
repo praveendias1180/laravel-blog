3b16921c46bb35253b7dc295723ce00e5f98687c
Praveen Dias <praveendias1180@gmail.com>
2021-12-16 18:52:15 +0530

    🚀 🚀Adding the Newsletter INTERFACE 🚀

--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -1,20 +1,9 @@
 <?php
 
 namespace App\Services;
-use MailchimpMarketing\ApiClient;
 
-class Newsletter {
+interface Newsletter{
 
-    protected $client;
+    public function subscribe(string $email, string $list = null);
 
-    public function __construct(ApiClient $client, string $foo){
-       $this->client = $client; 
-    }
-
-    public function subscribe(string $email, string $list = null){
-        $list ??= config('services.mailchimp.lists.subscribers');
-
-        $response = $this->client->lists->addListMember( $list, ['email_address' => $email, 'status' => 'subscribed']);
-        return $response;
-    }
 }
