3a69957019d5ce2e2fb1796123f02220588f1fca
Praveen Dias <praveendias1180@gmail.com>
2021-12-17 13:55:56 +0530

    ㊙️All REST actions are in one place. use Route:resource!

--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,11 @@ Route::post('session', [SessionController::class, 'store'])->middleware('guest')
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
 
 Route::middleware('can:admin')->group(function (){
-    Route::get('admin/posts', [AdminPostController::class, 'index']);
-    Route::post('admin/posts', [AdminPostController::class, 'store']);
-    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
-    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
-    Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
-    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
+    Route::resource('admin/posts', AdminPostController::class)->except('show');
+    // Route::get('admin/posts', [AdminPostController::class, 'index']);
+    // Route::post('admin/posts', [AdminPostController::class, 'store']);
+    // Route::get('admin/posts/create', [AdminPostController::class, 'create']);
+    // Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+    // Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
+    // Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
 });
