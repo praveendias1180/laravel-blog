81fd57fb83150cc5b20b4e877e0a3798241872dc
Praveen Dias <praveendias1180@gmail.com>
2021-12-17 10:30:36 +0530

    🚢New face for regiter page.

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -2,97 +2,37 @@
     <x-slot name="content">
         <section class="px-6 py-8">
         
-        <main class="max-w-lg mx-auto mt-20 border border-gray-200 bg-gray-100 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Register</h1>
-            <form method="POST" action="/register" class="mt-10">
-                @csrf
-                <!-- Name -->
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="name" >
-                        Name
-                    </lable>
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="name"
-                        id="name"
-                        value="{{ old("name") }}"
-                        required />
+        <main class="max-w-lg mx-auto mt-20">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Register</h1>
+                <form method="POST" action="/register" class="mt-10">
+                    @csrf
+                    <!-- Name -->
+                    <x-form.input name="name" />
 
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
+                    <!-- Username -->
+                    <x-form.input name="username" />
 
-                <!-- Username -->
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="username" >
-                        Username
-                    </lable>
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="username"
-                        id="username"
-                        value="{{ old("username") }}"
-                        required />
+                    <!-- Email -->
+                    <div class="mb-6">
+                    <x-form.input name="email" type="email" autocomplete="username"/>
 
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
+                    <!-- Password -->
+                    <x-form.input name="password" type="password" autocomplete="new-password" />
 
+                    <div class="mb-6">
+                        <x-form.button>Register</x-form.button>
+                    </div>
 
-                <!-- Email -->
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="email" >
-                        Email
-                    </lable>
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="email"
-                        id="email"
-                        value="{{ old("email") }}"
-                        required />
-
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                <!-- Password -->
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="password" >
-                        Password
-                    </lable>
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="password"
-                        name="password"
-                        id="password"
-                        required />
-
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <button type="submit"
-                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
-                        >
-                        Submit
-                    </button>
-                </div>
-
-                <!-- @if($errors->any())
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li class="text-red-500 text-xs">{{ $error }}</li>
-                    @endforeach
-                </ul>
-                @endif -->
-            </form>
+                    <!-- @if($errors->any())
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                    @endif -->
+                </form>
+            </x-panel>
         </main>
 
         </section>
